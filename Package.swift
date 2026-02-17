0fe265d233126e596c0e792a2973e1fe5f80372f
Gaston Kosut <gaston@leenspace.com>
2026-02-17 00:57:48 -0300

    Rename binary target to DeviceIntelligenceSDK to match module name

--- Package.swift
+++ Package.swift
@@ -7,11 +7,11 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        .library(name: "DeviceIntelligence", targets: ["DeviceIntelligence"])
+        .library(name: "DeviceIntelligenceSDK", targets: ["DeviceIntelligenceSDK"])
     ],
     targets: [
         .binaryTarget(
-            name: "DeviceIntelligence",
+            name: "DeviceIntelligenceSDK",
             url: "https://github.com/JuanLeenspace/device-intelligence-ios/releases/download/1.0.2/DeviceIntelligence.xcframework.zip",
             checksum: "20eb6188d7129f853d9b323042f41ed1c3265ad1e67f745ec0d720a5e656d5e9"
         )
